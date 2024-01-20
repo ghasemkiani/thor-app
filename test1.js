f5005c5e7a80f7b01bda686758c626783da34921
Ghasem Kiani <ghasemkiani@gmail.com>
2024-01-20 15:59:36 +0330

    update

--- test1.js
+++
@@ -1,6 +0,0 @@
-import {CryptoAmount, assetAmount, baseAmount, assetFromString, assetToBase, baseToAsset, register9Rheader} from "@xchainjs/xchain-util";
-
-let a = baseAmount(777.9, 5);
-
-console.log(a);
-console.log(a.amount().toString());
